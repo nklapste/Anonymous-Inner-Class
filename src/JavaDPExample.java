207e88c1a5ab1a6b9b59fe779e61264298a3df3c
nklapste <nklapste@ualberta.ca>
2017-11-27 13:54:02 -0700

    adding main files

---
+++ src/JavaDPExample.java
@@ -0,0 +1,72 @@
+/**
+ * Lab 6: Anonymous Inner Classes and Reflection <br />
+ * The {@code Animal} interface
+ */
+interface Animal {
+    /**
+     * An animal speaks
+     */
+    public void speak ();
+}
+
+/**
+ * Lab 6: Anonymous Inner Classes and Reflection <br />
+ * The {@code Lion} class
+ */
+class Lion implements Animal {
+    /**
+     * The lion speaks
+     */
+    public void speak() {
+        System.out.println("ROAR");
+    }
+}
+
+/**
+ * Lab 6: Anonymous Inner Classes and Reflection <br />
+ * The {@code Mouse} class
+ */
+class Mouse implements Animal {
+    /**
+     * The mouse speaks
+     */
+    public void speak() {
+        System.out.println("SQUEAK");
+    }
+}
+
+/**
+ * Lab 6: Anonymous Inner Classes and Reflection <br />
+ * The {@code Bison} class
+ */
+class Bison implements Animal {
+    /**
+     * The bison speaks
+     */
+    public void speak() {
+        System.out.println("BELLOW");
+    }
+}
+
+/**
+ * Lab 6: Anonymous Inner Classes and Reflection <br />
+ * The {@code AnimalType} class
+ */
+class AnimalType {
+    /**
+     * Create and return an animal
+     * @param criteria      {@code String} how is the animal like
+     * @return              {@code Animal} the animal
+     */
+    public static Animal getAnimal(String criteria) {
+        // TODO: Lab 6 Part 2-1 -- Refactor this method
+        if (criteria.equals("small"))
+            return new Mouse();
+        else if (criteria.equals("big"))
+            return new Bison();
+        else if (criteria.equals("lazy"))
+            return new Lion();
+        return null;
+    }
+}
+
