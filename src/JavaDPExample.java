8ce153fbd30b4b150073b69f274c59fb449e3978
nklapste <nklapste@ualberta.ca>
2017-11-28 11:42:54 -0700

    made generic reflective method for animal type

--- src/JavaDPExample.java
+++ src/JavaDPExample.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 /**
  * Lab 6: Anonymous Inner Classes and Reflection <br />
  * The {@code Animal} interface
@@ -52,21 +54,85 @@ class Bison implements Animal {
  * Lab 6: Anonymous Inner Classes and Reflection <br />
  * The {@code AnimalType} class
  */
-class AnimalType {
+abstract class AnimalType {
     /**
      * Create and return an animal
      * @param criteria      {@code String} how is the animal like
      * @return              {@code Animal} the animal
      */
-    public static Animal getAnimal(String criteria) {
-        // TODO: Lab 6 Part 2-1 -- Refactor this method
-        if (criteria.equals("small"))
-            return new Mouse();
-        else if (criteria.equals("big"))
-            return new Bison();
-        else if (criteria.equals("lazy"))
-            return new Lion();
-        return null;
+
+    private static HashMap<String,Animal> animalHashMap = new HashMap<>();
+
+    public <T extends Animal> void addAnimal(String criteria, Class<T> animal) {
+        try {
+            animalHashMap.put(criteria, animal.newInstance());
+        } catch (InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
+
+    public void removeCriteria(String criteria){
+        animalHashMap.remove(criteria);
+    }
+    public abstract void baseAnimals();
+
+    public AnimalType(){
+        animalHashMap = new HashMap<>();
+        baseAnimals();
+    }
+
+    public Animal getAnimal(String criteria) {
+        // TODO: Lab 6 Part 2-1 -- Refactor this method
+
+        // generate anonymous animal
+        if (!animalHashMap.containsKey(criteria)) {
+            return new Animal() {
+                @Override
+                public void speak() {
+                    System.out.println("unknown noise!");
+                }
+            };
+        }
+        try {
+            return (Animal) animalHashMap.get(criteria).getClass().newInstance();
+        } catch (InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        return new Animal() {
+            @Override
+            public void speak() {
+                System.out.println("unknown noise!");
+            }
+        };
+    }
+
+    public static void main(String[] args){
+
+        AnimalType a = new AnimalType(){
+            @Override
+            public void baseAnimals() {
+                animalHashMap.put("small", new Mouse());
+                animalHashMap.put("big", new Bison());
+                animalHashMap.put("lazy", new Lion());
+            }
+        };
+        a.getAnimal("small").speak();
+        a.addAnimal("predator", Lion.class);
+        a.getAnimal("predator").speak();
+
+        AnimalType b = new AnimalType(){
+            @Override
+            public void baseAnimals() {
+                animalHashMap.put("small", new Mouse());
+                animalHashMap.put("big", new Bison());
+                animalHashMap.put("lazy", new Lion());
+            }
+        };
+        b.getAnimal("predator").speak();
+
+
+    }
+
 }
 
